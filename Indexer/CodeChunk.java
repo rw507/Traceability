41c49f0517fec00f7af54b44f87149132a76ed6d
John <jca310ms@gmail.com>
2012-10-07 20:48:16 -0500

    HULU in the FUlU ...CTHUlU

--- Indexer/CodeChunk.java
+++ Indexer/CodeChunk.java
@@ -17,16 +17,15 @@ public class CodeChunk extends Chunk {
 	@Override
 	public boolean isComplete() {
 		int i = 1;
-		StringBuffer newBuffer = new StringBuffer();
-		boolean completed = false;
+		
 		char x = buffer.charAt(0);
 		char y = buffer.charAt(1);
 		for(;i<buffer.length();i++,x=y,y=buffer.charAt(i)){
 			
 			if(x == '/' && (y == '/' || y=='*')){
-				completed = true;
-				newBuffer.append(x);
-				newBuffer.append(y);
+			
+				buffer = buffer.delete(0,i-2);
+				return true;
 				
 			}
 		}
